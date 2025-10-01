fda8b520fdfd63123b17fcc2dba936af9c1f86ed
Satti Suma <154655364+sumareddy-1234@users.noreply.github.com>
2025-10-01 17:10:49 +0530

    Time: 2472 ms (5.01%), Space: 40.2 MB (94.11%) - LeetHub

---
+++ 0367-valid-perfect-square/0367-valid-perfect-square.java
@@ -0,0 +1,10 @@
+class Solution {
+    public boolean isPerfectSquare(int num) {
+        for(long i=1;i<=num;i++){
+            if(i*i==num){
+                return true;
+            }
+        }
+        return false;
+    }
+}
